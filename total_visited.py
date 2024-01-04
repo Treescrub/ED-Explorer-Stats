f84d97e6dd0cfa06e6d155006ab64d413989dfd8
Treescrub <treescrub@gmail.com>
2024-01-04 01:36:30 -0700

    Initial basic script to count visited systems

---
+++ total_visited.py
@@ -0,0 +1,47 @@
+import json
+import sys
+import argparse
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("saves_path", type=str, help="The path to the ED saved data")
+    
+    args = parser.parse_args()
+    saves_path = args.saves_path
+
+    journal_paths = []
+
+    for entry in os.listdir(saves_path):
+        entry_path = os.path.join(saves_path, entry)
+        
+        if os.path.isfile(entry_path) and entry.startswith("Journal.") and entry.endswith(".log"):
+            journal_paths.append(entry_path)
+
+    all_systems_visited = set()
+
+    for journal_path in journal_paths:
+        all_systems_visited.update(get_systems_visited(journal_path))
+    
+    print(len(all_systems_visited))
+                
+def get_systems_visited(journal_path):
+    visited_systems = set()
+    
+    with open(journal_path) as journal_file:
+        for line in journal_file:
+            event = json.loads(line)
+            
+            if event["event"] != "Scan":
+                continue
+            if event["ScanType"] != "AutoScan":
+                continue
+            if event["BodyName"] != event["StarSystem"]:
+                continue
+            
+            visited_systems.add(event["StarSystem"])
+    
+    return visited_systems
+
+if __name__ == "__main__":
+    main()
